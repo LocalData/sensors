230cc846614e46606b5ac8aff4ab6a4232467734
Prashant Singh <prashant@localdata.com>
2014-12-19 03:39:26 -0800

    Require bearer tokens for creating entries

--- scripts/create-table.sql
+++ scripts/create-table.sql
@@ -1,3 +1,5 @@
 CREATE TABLE entries (source character(25), ts timestamptz, data json);
 CREATE INDEX ON entries (source);
 CREATE INDEX ON entries (ts);
+
+CREATE TABLE sources (source character(25) PRIMARY KEY, token text UNIQUE, data json);
